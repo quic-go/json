2c01c1d2828d2d6ab890d6d0ca6ef4b2aa9807a5
Marten Seemann <martenseemann@gmail.com>
2025-10-01 15:21:01 +0800

    remove test that uses Go 1.25 features

--- v2_encode_test.go
+++ v2_encode_test.go
@@ -14,9 +14,7 @@ import (
 	"regexp"
 	"runtime/debug"
 	"strconv"
-	"sync"
 	"testing"
-	"testing/synctest"
 	"time"
 )
 
@@ -1407,22 +1405,4 @@ func TestIssue63379(t *testing.T) {
 			t.Errorf("expected error for %q", v)
 		}
 	}
-}
-
-// Issue #73733: encoding/json used a WaitGroup to coordinate access to cache entries.
-// Since WaitGroup.Wait is durably blocking, this caused apparent deadlocks when
-// multiple bubbles called json.Marshal at the same time.
-func TestSynctestMarshal(t *testing.T) {
-	var wg sync.WaitGroup
-	for range 5 {
-		wg.Go(func() {
-			synctest.Test(t, func(t *testing.T) {
-				_, err := Marshal([]string{})
-				if err != nil {
-					t.Errorf("Marshal: %v", err)
-				}
-			})
-		})
-	}
-	wg.Wait()
 }
